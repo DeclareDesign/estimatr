dc6717cf13ea656811d2af0a638f58472d7803b1
Graeme Blair <graemeblair@users.noreply.github.com>
2017-03-23 23:11:34 -0700

    names

---
+++ R/DDestimate.R
@@ -0,0 +1,8 @@
+#' DDestimate package
+#'
+#' Some stuff about what this package is
+#'
+#' @docType package
+#' @useDynLib DDestimate
+#' @name DDestimate
+NULL
--- R/estimatorpack.R
+++
@@ -1,9 +0,0 @@
-#' estimatorpack package
-#'
-#' Some stuff about what this package is
-#'
-#' @docType package
-#' @importFrom Rcpp evalCpp
-#' @useDynLib estimatorpack
-#' @name estimatorpack
-NULL
