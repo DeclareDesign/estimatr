effa0ac0192954ab3c35e92c2046848ebdb00880
Luke Sonnet <luke.sonnet@gmail.com>
2017-08-14 22:54:29 +0500

    Explicitly export fns

--- R/estimatr.R
+++ R/estimatr.R
@@ -6,6 +6,7 @@
 #' @useDynLib estimatr, .registration = TRUE
 #' @importFrom Rcpp evalCpp
 #' @importFrom stats sd var model.matrix.default pt qt var weighted.mean lm vcov
+#' @export _estimatr_mult_diag _estimatr_sqrt_inv _estimatr_lm_robust_helper
 #' @name estimatr
 NULL
 
