7ae08b616a0f41605784d13432f697b40a1f4d0a
Luke Sonnet <luke.sonnet@gmail.com>
2017-08-20 14:03:18 -0700

    Allowed missingness in weights; added better tests for missingness and WLS #16

--- tests/testthat/test-lm-robust.R
+++ tests/testthat/test-lm-robust.R
@@ -33,7 +33,10 @@ context("lm robust se")
 
 test_that("lm robust works with missingness",{
 
-  df <- data.frame(Y = rnorm(100), Z = rbinom(100, 1, .5), X = rnorm(100), W = runif(100))
+  df <- data.frame(Y = rnorm(100),
+                   Z = rbinom(100, 1, .5),
+                   X = rnorm(100),
+                   W = runif(100))
 
   df$X[23] <- NA
 
@@ -45,5 +48,56 @@ test_that("lm robust works with missingness",{
   lm_robust(Y ~ Z + X, coefficient_name = c("Z", "X"), data = df)
   lm_robust(Y ~ Z + X, coefficient_name = c("(Intercept)", "Z", "X"), data = df)
   lm_robust(Y ~ Z*X, coefficient_name = "Z:X", data = df)
+
+})
+
+
+context("lm robust se")
+
+test_that("lm robust works with weights",{
+
+  df <- data.frame(Y = rnorm(100),
+                   Z = rbinom(100, 1, .5),
+                   X = rnorm(100),
+                   W = runif(100))
+
+  ## Make sure weighting works
+  expect_error(
+    estimatr_out <- lm_robust(Y ~ Z * X, weights = W, data = df),
+    NA
+  )
+
+  # Compare to lm output
+  lm_out <- lm(Y ~ Z * X, weights = W, data = df)
+  lmo_hc2 <- cbind(lm_out$coefficients,
+                   sqrt(diag(sandwich::vcovHC(lm_out, type = 'HC2'))))
+
+  expect_equivalent(
+    as.matrix(estimatr_out[, c('est', 'se')]),
+    lmo_hc2
+  )
+
+  ## Make sure weighting works with missingness
+  df$W[39] <- NA
+
+  expect_warning(
+    estimatr_miss_out <- lm_robust(Y ~ Z * X, weights = W, data = df),
+    'missing'
+  )
+
+  expect_identical(
+    estimatr_miss_out,
+    lm_robust(Y ~ Z * X, weights = W, data = df[-39, ])
+  )
+
+  # Compare to lm output
+  lm_miss_out <- lm(Y ~ Z * X, weights = W, data = df)
+  lmo_miss_hc2 <- cbind(lm_miss_out$coefficients,
+                        sqrt(diag(sandwich::vcovHC(lm_miss_out, type = 'HC2'))))
+
+  expect_equivalent(
+    as.matrix(estimatr_miss_out[, c('est', 'se')]),
+    lmo_miss_hc2
+  )
 
 })
