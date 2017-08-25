399f05be5785a2ecd1c2768108471df0b17cce43
Luke Sonnet <luke.sonnet@gmail.com>
2017-08-25 10:42:34 -0700

    Test replication of Lin

--- data-raw/build-alo-star-for-lin.R
+++ data-raw/build-alo-star-for-lin.R
@@ -1,6 +1,10 @@
-## This script takes the dta file from the ALO replication data at this url:
-## https://www.aeaweb.org/articles?id=10.1257/app.1.1.136
-## and turns it in to the .rda we export with the package
+# This script takes the dta file from the ALO replication data at this url:
+# https://www.aeaweb.org/articles?id=10.1257/app.1.1.136
+# and turns it in to the .rda we export with the package
+# Full citation:
+# Angrist, Joshua, Daniel Lang, and Philip Oreopoulos. 2009. “Incentives and
+# Services for College Achievement: Evidence from a Randomized Trial.” American
+# Economic Journal: Applied Economics 1(1): 136–63.
 
 # wd should be package root
 
