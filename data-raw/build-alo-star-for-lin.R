c8c603bafdc14bad71b84a9ed6765985048adeb5
Luke Sonnet <luke.sonnet@gmail.com>
2017-08-25 10:03:06 -0700

    Add ALO STAR male data for Lin 2013 replication

---
+++ data-raw/build-alo-star-for-lin.R
@@ -0,0 +1,21 @@
+## This script takes the dta file from the ALO replication data at this url:
+## https://www.aeaweb.org/articles?id=10.1257/app.1.1.136
+## and turns it in to the .rda we export with the package
+
+# wd should be package root
+
+dat <-
+  foreign::read.dta(
+    'data-raw/STAR_public_use.dta'
+  )
+
+alo_star_men <- dat[dat$sex == 'M' &
+                      !is.na(dat$GPA_year1) &
+                      (dat$sfsp == 1 | dat$ssp == 1) &
+                      dat$noshow == 0,
+                    c('gpa0', 'sfsp', 'ssp', 'sfp', 'GPA_year1', 'GPA_year2')
+                    ]
+
+devtools::use_data(alo_star_men,
+                   internal = F,
+                   overwrite = T)
