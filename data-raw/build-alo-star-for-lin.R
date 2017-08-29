f4c67fee8e71abd1bd9d8c00b725b5fdc65118db
Luke Sonnet <luke.sonnet@gmail.com>
2017-08-29 09:07:23 -0700

    Additional documentation

--- data-raw/build-alo-star-for-lin.R
+++ data-raw/build-alo-star-for-lin.R
@@ -2,8 +2,8 @@
 # https://www.aeaweb.org/articles?id=10.1257/app.1.1.136
 # and turns it in to the .rda we export with the package
 # Full citation:
-# Angrist, Joshua, Daniel Lang, and Philip Oreopoulos. 2009. “Incentives and
-# Services for College Achievement: Evidence from a Randomized Trial.” American
+# Angrist, Joshua, Daniel Lang, and Philip Oreopoulos. 2009. "Incentives and
+# Services for College Achievement: Evidence from a Randomized Trial." American
 # Economic Journal: Applied Economics 1(1): 136–63.
 
 # wd should be package root
@@ -17,7 +17,7 @@ alo_star_men <- dat[dat$sex == 'M' &
                       !is.na(dat$GPA_year1) &
                       (dat$sfsp == 1 | dat$ssp == 1) &
                       dat$noshow == 0,
-                    c('gpa0', 'sfsp', 'ssp', 'sfp', 'GPA_year1', 'GPA_year2')
+                    c('gpa0', 'sfsp', 'ssp', 'GPA_year1', 'GPA_year2')
                     ]
 
 devtools::use_data(alo_star_men,
