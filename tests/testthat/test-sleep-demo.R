4fed951fe915c1ecb110fa68c42603da0813630d
Neal Fultz <nfultz@gmail.com>
2017-10-20 08:46:43 -0700

    demo 2 test

--- tests/testthat/test-sleep-demo.R
+++
@@ -1,9 +0,0 @@
-context("sleep_demo")
-
-
-test_that("lm_robust works when called from global environment", {
-  expect_output_file(
-    demo("sleep", package = "estimatr"),
-    file = "sleep-demo.txt"
-  )
-})
