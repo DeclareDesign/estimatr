ecef7181b9a0eaecfe0aa12de6da0397fb70c238
Neal Fultz <nfultz@gmail.com>
2018-03-20 15:34:40 +0000

    fix tests

--- R/helper_na_omit_detailed.R
+++ R/helper_na_omit_detailed.R
@@ -12,7 +12,7 @@ na.omit_detailed.data.frame <- function(object){
 
   why_omit <- naomitwhy(object, function(x) is.na(x))
 
-  if(is.integer(why_omit)) {
+  if(length(why_omit)) {
     object <- if(length(dim(object))) object[-why_omit, , drop=FALSE] else object[-why_omit]
     attr(object, "na.action") <- why_omit
   }
