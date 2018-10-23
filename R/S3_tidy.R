a38db4e36f7b51150e5268aee6e20a554075213f
Luke Sonnet <luke.sonnet@gmail.com>
2018-10-23 16:02:57 -0700

    remove broom; add generics

--- R/S3_tidy.R
+++ R/S3_tidy.R
@@ -1,91 +1,19 @@
 # some code taken from the "broom" package
 # https://github.com/tidyverse/broom
 
-#' Tidy the result of an estimator into a data.frame
-#' @rdname tidy
-#'
+#' @importFrom generics tidy
 #' @export
-tidy <- function(object, ...) {
-  if (requireNamespace("broom", quietly = TRUE))
-    broom::tidy(object, ...)
-  else
-    UseMethod("tidy")
-}
-
-
-#' @rdname tidy
-#'
-#' @param object An object returned by one of the estimators
-#' @param ... extra arguments (not used)
-#'
-#' @export
-tidy.NULL <- function(object, ...) {
-  data.frame()
-}
-
-#' @rdname tidy
-#'
-#' @export
-tidy.default <- function(object, ...) {
-  warning(paste(
-    "No method for tidying an S3 object of class",
-    class(object),
-    ", using as.data.frame"
-  ))
-  as.data.frame(object)
-}
-
-
-#' @rdname tidy
-#'
-#' @return A data.frame with with coefficient names, estimates, standard
-#' errors, confidence intervals, p-values, degrees of freedom, and the
-#' name of the outcome variable
-#'
-#' @export tidy.lm_robust
-#' @export
-tidy.lm_robust <- function(object, ...) {
-  warn_singularities(object)
-  tidy_data_frame(object)
-}
-
-#' @rdname tidy
-#'
-#' @export tidy.iv_robust
-#' @export
-tidy.iv_robust <- function(object, ...) {
-  warn_singularities(object)
-  tidy_data_frame(object)
-}
-
-
-#' @rdname tidy
-#'
-#' @export tidy.difference_in_means
-#' @export
-tidy.difference_in_means <- function(object, ...) {
-  tidy_data_frame(object)
-}
-
-
-#' @rdname tidy
-#'
-#' @export tidy.horvitz_thompson
-#' @export
-tidy.horvitz_thompson <- function(object, ...) {
-  tidy_data_frame(object)
-}
-
+generics::tidy
 
 tidy_data_frame <- function(object, digits = NULL) {
   vec_cols <- c(
-      "coefficients",
-      "std.error",
-      "statistic",
-      "p.value",
-      "conf.low",
-      "conf.high",
-      "df"
+    "coefficients",
+    "std.error",
+    "statistic",
+    "p.value",
+    "conf.low",
+    "conf.high",
+    "df"
   )
 
   tidy_mat <- do.call("cbind", lapply(object[vec_cols], as.vector))
@@ -100,7 +28,7 @@ tidy_data_frame <- function(object, digits = NULL) {
   return_frame$outcome <- rep(object[["outcome"]], each = length(object[["term"]]))
 
   rownames(return_frame) <- NULL
-  return(return_frame)
+  tibble::tibble(return_frame)
 }
 
 warn_singularities <- function(object) {
@@ -112,4 +40,52 @@ warn_singularities <- function(object) {
       " not defined because the design matrix is rank deficient\n"
     )
   }
+}
+
+#' Tidy an estimatr object
+#' @name estimatr_tidiers
+#' @templateVar class lm_robust
+#' @return A [tibble::tibble()] with columns for coefficient names, estimates, standard
+#' errors, confidence intervals, p-values, degrees of freedom, and the
+#' name of the outcome variable
+#'
+#' @param object An object returned by one of the estimators
+#' @param ... extra arguments (not used)
+#'
+#' @export
+#' @seealso [tidy()], [estimatr::lm_robust()], [estimatr::iv_robust()],  [estimatr::difference_in_means()], [estimatr::horvitz_thompson()]
+#' @family estimatr tidiers
+tidy.lm_robust <- function(object, ...) {
+  warn_singularities(object)
+  tidy_data_frame(object)
+}
+
+#' @rdname estimatr_tidiers
+#' @templateVar class iv_robust
+#'
+#' @export
+#' @family estimatr tidiers
+tidy.iv_robust <- function(object, ...) {
+  warn_singularities(object)
+  tidy_data_frame(object)
+}
+
+#' @rdname estimatr_tidiers
+#' @templateVar class difference_in_means
+#'
+#' @export
+#' @family estimatr tidiers
+tidy.difference_in_means <- tidy_data_frame
+
+#' @rdname estimatr_tidiers
+#' @templateVar class horvitz_thompson
+#'
+#' @export
+#' @family estimatr tidiers
+tidy.horvitz_thompson <- tidy_data_frame
+
+#' @export
+#' @family estimatr tidiers
+tidy.NULL <- function(object, ...) {
+  tibble::tibble()
 }
---
+++ tests/testthat/helper-return-cleaners.R
@@ -0,0 +1,13 @@
+# This fn removes calls from function returns to make testing easier
+rmcall <- function(obj) {
+  if (!is.null(obj[["call"]])) {
+    obj[["call"]] <- NULL
+  }
+  return(obj)
+}
+
+# This fn casts tibbles as data.frames for equivalency tests
+# TODO implement this everywhere
+expect_equivalent_tbl <- function(obj1, obj2) {
+  expect_equivalent(as.data.frame(obj1), as.data.frame(obj2))
+}
