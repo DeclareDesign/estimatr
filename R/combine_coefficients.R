c117c590e579fece7ff41b87c032aa7ea4ed48a0
Lily Medina <lilymiru@gmail.com>
2019-01-14 12:39:17 +0100

    Revert "work in progress combine_coefficients"
    This reverts commit 07646c31f9e211fe8d55db9b212bd2c7d3d994a4.

--- R/combine_coefficients.R
+++ R/combine_coefficients.R
@@ -1,49 +1,11 @@
+combine_coefficients <- function(model, combined_coefficients, level = 0.95, ...){
 
+ out <- car::linearHypothesis(model, combined_coefficients)
 
-extract_terms <- function(x){
-  x <- gsub("*", "", x, fixed=TRUE)
-  x <- gsub(" ", "", x, fixed=TRUE)
-  x <- gsub("\\n", " ", x)
-  x <- gsub("\\t", " ", x)
-  x <- gsub("-", "+-", x, fixed = TRUE)
-  x <- strsplit(x, "+", fixed = TRUE)[[1]]
-  x[x!=""]
-}
-
-
-extract_coefficients <- function( combination, coefnames){
- comb_vector <- extract_terms(combination)
- terms <- gsub("^[-\\ 0-9]+", "", comb_vector)
- i <- coefnames %in%  terms
- if( length(terms) > sum(i)) stop("Linear combination is")
- a <- mapply(function(x, y) unlist(strsplit(y, x, fixed=TRUE)),  terms, comb_vector) #split hypothesis
- a[a == ""] <- 1
- a[a == "-"] <- -1
-
- out <- coefnames
- names(out) <- out
- out[names(a)] <- a
- out[!i] <- 0
- out <- as.numeric(out)
- names(out) <- coefnames
- out
-}
-
-combined_se <- function(l, vcov){
-  vcov_lb <-  t(l) %*%  vcov(model) %*% l
-  sqrt(diag(vcov_lb))
-}
-
-combined_estimate <- function(l, coeffs){
-  sum(l*coeffs)
-}
-
-
-combine_coefficients <- function(model, combination, level = 0.95, ...){
- coeffs <- coefficients(model)
- l <- sapply(combination,extract_coefficients, coefnames)
- estimate  <- apply(l, 2, combined_estimate, coeffs)
- std.error <- apply(l, 2, combined_se , coeffs)
+ n <- nrow(model$model)
+ p <- nrow(out) - 1
+ estimate  <- drop(attr(out, "value"))
+ std.error <- sqrt(diag(attr(out, "vcov")))
  df <- model$df.residual
  tt <- estimate/std.error
  p.value <-  2 * pt(abs(tt), df)
@@ -52,6 +14,7 @@ combine_coefficients <- function(model, combination, level = 0.95, ...){
  ci <- estimate + std.error %o% qt(alpha, df)
  conf.low  <-  ci[,1]
  conf.high <-  ci[,2]
+ outcome <- rownames(attr(model$terms, "factors"))[1]
 
  data.frame(estimate = estimate,
             std.error =  std.error,
