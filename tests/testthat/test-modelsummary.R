465bc2ca62d0b5c95b3d05a91f066047a1cf84bb
Graeme Blair <graemeblair@users.noreply.github.com>
2019-09-06 14:55:30 -0400

    modelsummary, simpler test

--- tests/testthat/test-modelsummary.R
+++ tests/testthat/test-modelsummary.R
@@ -1,37 +1,41 @@
-# This file is ignored by .Rbuildignore to keep from suggesting gt and gtsummary
+# This file is ignored by .Rbuildignore to keep from suggesting gt and modelsummary
 
-context("S3 - gtsummary works")
+context("S3 - modelsummary works")
 
-test_that("gtsummary works with glance", {
+test_that("modelsummary works with glance", {
 
-  library(gt)
-  library(gtsummary)
+  library(modelsummary)
 
   model1 <- lm_robust(mpg ~ am, mtcars)
   model2 <- lm_robust(mpg ~ am, mtcars, clusters = cyl)
   model3 <- lm_lin(mpg ~ am, ~ cyl, mtcars)
 
-  gto <- gtsummary(list(model1, model2, model3))
+  gto <- modelsummary(list(model1, model2, model3))
 
-  expect_equal(colnames(gto), c("Model 1", "Model 2", "Model 3"))
-
-  expect_equal(nrow(gto), 15L)
+  expect_s3_class(gto, "gt_tbl")
 
   # iv_robust
   model1 <- iv_robust(mpg ~ am | gear, mtcars)
   model2 <- iv_robust(mpg ~ am | gear, mtcars, clusters = cyl, diagnostics = TRUE)
 
-  gtsummary(list(model1, model2), gof_omit = c("N|[sS]tatistic|p.value|p{1}"))
+  gto <- modelsummary(list(model1, model2), gof_omit = c("N|[sS]tatistic|p.value|p{1}"))
+
+  expect_s3_class(gto, "gt_tbl")
 
   # difference_in_means
   model1 <- difference_in_means(mpg ~ am, mtcars)
   model2 <- difference_in_means(mpg ~ am, mtcars, blocks = vs)
 
-  gtsummary(list(model1, model2))
+  gto <- modelsummary(list(model1, model2))
+
+  expect_s3_class(gto, "gt_tbl")
 
   # horvitz_thompson
   model1 <- horvitz_thompson(mpg ~ am, mtcars)
   model2 <- horvitz_thompson(mpg ~ am, mtcars, blocks = vs)
 
-  gtsummary(list(model1, model2))
+  gto <- modelsummary(list(model1, model2))
+
+  expect_s3_class(gto, "gt_tbl")
+
 })
