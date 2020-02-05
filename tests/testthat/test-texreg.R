7b0abab9455666dccb7a9d580bf3069dfec02a4c
Luke Sonnet <luke.sonnet@gmail.com>
2020-02-05 08:38:27 +0100

    Add n clusters to extract.lm_robust

--- tests/testthat/test-texreg.R
+++ tests/testthat/test-texreg.R
@@ -6,7 +6,7 @@ test_that("texreg extension works", {
 
   model2 <- lm_robust(extra~group, sleep, clusters = ID)
 
-  capture.output(treg <- extract(model2, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE))
+  capture.output(treg <- extract(model2, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE))
   expect_is(
     treg,
     "texreg"
@@ -17,5 +17,8 @@ test_that("texreg extension works", {
 
   # Remove to get SEs
   expect_true(grepl("\\(0.57\\)", texreg::texreg(model2, include.ci = FALSE)))
+
+  # Has Nclust
+  expect_true(grepl("N Clusters.*10", texreg::texreg(model2)))
 
 })
